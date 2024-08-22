7aed40a4d05e6f9e1b69479a1c91f7e9c1f7e34d
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-22 18:24:39 +0800

    Feat: CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/91APP_front-end-class",
   plugins: [react()],
-})
+});
