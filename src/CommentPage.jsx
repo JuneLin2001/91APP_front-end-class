32d080aa49d8346e473ac2c5bc06f23414edeaca
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-08-23 17:09:44 +0800

    add:get comments list

--- src/CommentPage.jsx
+++ src/CommentPage.jsx
@@ -4,35 +4,10 @@ function CommentPage() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [text, setText] = useState('');
+
   const owner = 'JuneLin2001';
   const repo = '91APP_front-end-class';
   const issue_number = 1;
-
-  const handleCreateComment = async () => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`,
-        {
-          method: 'POST',
-          headers: {
-            Authorization: 'Bearer YOUR_GITHUB_TOKEN',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ body: { text } }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Failed to create comment');
-      }
-
-      const data = await response.json();
-      console.log('Comment created:', data);
-    } catch (error) {
-      console.error('Error creating comment:', error);
-    }
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -61,19 +36,6 @@ function CommentPage() {
   if (error) return <div>錯誤: {error}</div>;
   if (!data) return <div>無數據</div>;
 
-  //   const { Octokit } = require("@octokit/rest");
-  //   const octokit = new Octokit({
-  //     auth: "YOUR_PERSONAL_ACCESS_TOKEN",
-  //   });
-
-  //   await octokit.request('GET /repos/{owner}/{repo}/issues/comments', {
-  //   owner: 'JuneLin2001',
-  //   repo: '91APP_front-end-class',
-  //   headers: {
-  //     'X-GitHub-Api-Version': '2022-11-28'
-  //   }
-  // })
-
   return (
     <>
       <div>
@@ -82,18 +44,6 @@ function CommentPage() {
             <p>{comment.body}</p>
           </div>
         ))}
-      </div>
-
-      <div>
-        <textarea
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Add your comment here..."
-          rows="4"
-          cols="50"
-        />
-        <br />
-        <button onClick={handleCreateComment}>Comment</button>
       </div>
     </>
   );
