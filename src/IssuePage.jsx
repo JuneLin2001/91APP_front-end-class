c79672d527ced645907dd98a25d03f4d14deb2f2
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-23 13:58:07 +0800

    feat: get issue and auther and lebel from api

--- src/IssuePage.jsx
+++ src/IssuePage.jsx
@@ -1,5 +1,41 @@
+import { useState, useEffect } from "react";
+
 const IssuePage = () => {
-  return <h1>Issue Page</h1>;
+  const [apiResult, setApiResult] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.github.com/repos/JuneLin2001/91APP_front-end-class/issues"
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setApiResult(responseJson);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Issue Page</h1>
+      <ul>
+        {apiResult.map(
+          (issue) =>
+            !issue.pull_request && (
+              <li key={issue.id}>
+                <a href={issue.html_url}>{issue.title}</a>
+                <p>創建時間: {new Date(issue.created_at).toLocaleString()}</p>
+                <p>發布者: {issue.user.login}</p>
+                <ul>
+                  {issue.labels.map((label) => (
+                    <li key={label.id}>{label.name}</li>
+                  ))}
+                </ul>
+              </li>
+            )
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default IssuePage;
