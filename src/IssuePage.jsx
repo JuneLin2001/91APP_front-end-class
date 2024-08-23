e9de4c45adb2ae28d415ab0a18caa212c236b1c5
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-23 16:12:48 +0800

    feat: search (include title and body)

--- src/IssuePage.jsx
+++ src/IssuePage.jsx
@@ -6,6 +6,9 @@ const IssuePage = () => {
   const [labels, setLabels] = useState([]);
   const [selectedAuthor, setSelectedAuthor] = useState("all");
   const [selectedLabel, setSelectedLabel] = useState("all");
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     fetch(
@@ -33,12 +36,46 @@ const IssuePage = () => {
   }, []);
 
   const handleAuthorChange = (e) => {
+    setIsSearching(false);
     setSelectedAuthor(e.target.value);
   };
 
   const handleLabelChange = (e) => {
+    setIsSearching(false);
     setSelectedLabel(e.target.value);
   };
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearchClick = (e) => {
+    e.preventDefault();
+    setIsSearching(true);
+
+    fetch(
+      `https://api.github.com/search/issues?q=repo:JuneLin2001/91APP_front-end-class ${searchValue}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResult(data.items);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const filteredIssues = (issues) =>
+    issues
+      .filter(
+        (issue) =>
+          (selectedAuthor === "all" || issue.user.login === selectedAuthor) &&
+          (selectedLabel === "all" ||
+            issue.labels.some((label) => label.name === selectedLabel))
+      )
+      .filter((issue) => !issue.pull_request); // 排除 pull requests
+
+  const issuesToDisplay = isSearching
+    ? searchResult
+    : filteredIssues(apiResult);
 
   return (
     <div>
@@ -64,28 +101,28 @@ const IssuePage = () => {
         ))}
       </select>
 
+      <form>
+        <input
+          placeholder="is:issue is:open"
+          value={searchValue}
+          onChange={handleSearchChange}
+        ></input>
+        <button onClick={handleSearchClick}>搜尋</button>
+      </form>
       <ul>
-        {apiResult
-          .filter(
-            (issue) =>
-              (selectedAuthor === "all" ||
-                issue.user.login === selectedAuthor) &&
-              (selectedLabel === "all" ||
-                issue.labels.some((label) => label.name === selectedLabel))
-          )
-          .map(
-            (issue) =>
-              !issue.pull_request && (
-                <li key={issue.id}>
-                  <a href={issue.html_url}>{issue.title}</a>
-                  <p>創建時間: {new Date(issue.created_at).toLocaleString()}</p>
-                  <p>發布者: {issue.user.login}</p>
-                  {issue.labels.map((label) => (
-                    <p key={label.id}>{label.name}</p>
-                  ))}
-                </li>
-              )
-          )}
+        {issuesToDisplay.map(
+          (issue) =>
+            !issue.pull_request && (
+              <li key={issue.id}>
+                <a href={issue.html_url}>{issue.title}</a>
+                <p>創建時間: {new Date(issue.created_at).toLocaleString()}</p>
+                <p>發布者: {issue.user.login}</p>
+                {issue.labels.map((label) => (
+                  <p key={label.id}>{label.name}</p>
+                ))}
+              </li>
+            )
+        )}
       </ul>
     </div>
   );
