e22d4b3f3312e25d5d77b63255dd5acc54e511b3
Sylvie <yuanqi1368@example.com>
2024-08-26 09:10:58 +0800

    fix: update router paths for  Firebase deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import IssuePage from './IssuePage.jsx';
-import GitHubLogin from './utils/GitHubLogin';
-import CommentPage from './CommentPage.jsx';
-import { AuthContextProvider } from './context/authContext';
+import { Routes, Route } from "react-router-dom";
+import IssuePage from "./IssuePage.jsx";
+import GitHubLogin from "./utils/GitHubLogin";
+import CommentPage from "./CommentPage.jsx";
+import { AuthContextProvider } from "./context/authContext";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
         <Routes>
-          <Route
-            path="/91APP_front-end-class/login"
-            element={<GitHubLogin />}
-          />
-          <Route path="/91APP_front-end-class/issue" element={<IssuePage />} />
-          <Route
-            path="/91APP_front-end-class/comment"
-            element={<CommentPage />}
-          />
+          <Route path="/login" element={<GitHubLogin />} />
+          <Route path="/issue" element={<IssuePage />} />
+          <Route path="/comment" element={<CommentPage />} />
         </Routes>
       </AuthContextProvider>
     </>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/91APP_front-end-class",
+  // base: "/91APP_front-end-class",
+  base: "./",
   plugins: [react()],
 });
