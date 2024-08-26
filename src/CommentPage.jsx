1ab5b0e36063939d6274b9765e109d51eb5b8a6c
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-08-26 11:19:19 +0800

    feat: temp

--- src/CommentPage.jsx
+++ src/CommentPage.jsx
@@ -1,12 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Timeline,
+  Avatar,
+  Text,
+  Label,
+  ActionMenu,
+  ActionList,
+  IconButton,
+  ThemeProvider,
+  RelativeTime,
+} from "@primer/react";
 
+import { KebabHorizontalIcon } from "@primer/octicons-react";
 function CommentPage() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const owner = 'JuneLin2001';
-  const repo = '91APP_front-end-class';
+  const owner = "rebeccaS47";
+  const repo = "Wordle";
   const issue_number = 1;
 
   useEffect(() => {
@@ -20,7 +33,7 @@ function CommentPage() {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const result = await response.json();
-        console.log('fetch到的資料', result);
+        console.log("fetch到的資料", result);
         setData(result);
       } catch (e) {
         setError(e.message);
@@ -37,15 +50,94 @@ function CommentPage() {
   if (!data) return <div>無數據</div>;
 
   return (
-    <>
-      <div>
+    <ThemeProvider>
+      <Timeline>
         {data.map((comment) => (
-          <div key={comment.id}>
-            <p>{comment.body}</p>
-          </div>
+          // <Timeline.Item key={comment.id}>
+          //   <Timeline.Badge>
+          //     <Avatar src={comment.user.avatar_url} alt="User Avatar" />
+          //   </Timeline.Badge>
+          //   <Timeline.Body>
+          //     <Box>
+          //       <Text fontWeight="bold">{comment.user.login}</Text>
+          //       <Text fontSize={1} color="fg.muted">
+          //         {' '}
+          //         commented yesterday • edited{' '}
+          //       </Text>
+          //     </Box>
+          //     <Text>{comment.body}</Text>
+          //   </Timeline.Body>
+          // </Timeline.Item>
+          <Timeline.Item key={comment.id}>
+            <Avatar size={40} src={comment.user.avatar_url} alt="User Avatar" />
+            <Timeline.Body ml={6} borderColor-accent-muted borderWidth={1}>
+              <Box>
+                <Text>{comment.user.login}</Text>
+                {/* <Text>commented{comment.updated_at}</Text> */}
+                <RelativeTime date={new Date(comment.updated_at)} />
+                <Label>{comment.author_association}</Label>
+                <Label>Author</Label>
+                <ActionMenu>
+                  <ActionMenu.Anchor>
+                    <IconButton
+                      icon={KebabHorizontalIcon}
+                      unsafeDisableTooltip={false}
+                      variant="invisible"
+                    />
+                  </ActionMenu.Anchor>
+
+                  <ActionMenu.Overlay width="medium">
+                    <ActionList>
+                      <ActionList.Item
+                        onSelect={() => alert("Copy link clicked")}
+                      >
+                        Copy link
+                      </ActionList.Item>
+                      <ActionList.Item
+                        onSelect={() => alert("Quote reply clicked")}
+                      >
+                        Quote reply
+                      </ActionList.Item>
+                      <ActionList.Item
+                        onSelect={() => alert("Reference in new issue clicked")}
+                      >
+                        Reference in new issue
+                      </ActionList.Item>
+                      <ActionList.Divider />
+                      <ActionList.Item
+                        onSelect={() => alert("Edit comment clicked")}
+                      >
+                        Edit
+                      </ActionList.Item>
+                      <ActionList.Item
+                        onSelect={() => alert("Hide comment clicked")}
+                      >
+                        Hide
+                      </ActionList.Item>
+                      <ActionList.Item
+                        variant="danger"
+                        onSelect={() => alert("Delete comment clicked")}
+                      >
+                        Delete
+                      </ActionList.Item>
+                      <ActionList.Divider />
+                      <ActionList.Item
+                        onSelect={() => alert("Delete file clicked")}
+                      >
+                        Report content
+                      </ActionList.Item>
+                    </ActionList>
+                  </ActionMenu.Overlay>
+                </ActionMenu>
+              </Box>
+              <Box>
+                <Text>{comment.body}</Text>
+              </Box>
+            </Timeline.Body>
+          </Timeline.Item>
         ))}
-      </div>
-    </>
+      </Timeline>
+    </ThemeProvider>
   );
 }
 
