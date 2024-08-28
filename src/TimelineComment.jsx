2c229437bc93bb82796a6463ef8be0e2552d6b3e
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-08-28 16:45:34 +0800

    feat: extract timeline comment from CommentPage component into TimelineComment component.

--- src/TimelineComment.jsx
+++ src/TimelineComment.jsx
@@ -1,6 +1,19 @@
-import { Box, Text, Avatar, Timeline, ActionMenu, ActionList, Button } from "@primer/react";
+import {
+  Box,
+  Text,
+  Avatar,
+  Timeline,
+  ActionMenu,
+  ActionList,
+  Button,
+  RelativeTime,
+  Label,
+  Link,
+} from "@primer/react";
 import { KebabHorizontalIcon, SmileyIcon } from "@primer/octicons-react";
-import { withTheme } from "styled-components";
+import { useContext } from "react";
+import { CommentContext } from "./context/commentContext";
+import CommentBox from "./comment";
 
 const getHeaderColor = (role) => {
   switch (role) {
@@ -11,72 +24,91 @@ const getHeaderColor = (role) => {
   }
 };
 
-const CommentItem = ({
-  user,
-  role,
-  time,
-  content,
-  isEdited,
-  isDeleted,
-  editedBy,
-}) => {
+const TimelineComment = () => {
+  const {
+    commentData,
+    editingCommentId,
+    currentTextareaValue,
+    handleDelete,
+    handleUpdate,
+    handleTextareaChange,
+    setEditingCommentId,
+  } = useContext(CommentContext);
+
   return (
-    <>
-      <Timeline.Item
-        sx={{
-          marginLeft: "0px",
-          "::before": {
-            left: "20px",
-            zIndex: "-1 ",
-          },
-        }}
-      >
-        <Timeline.Badge
-          sx={{ position: "absolute", left: "-30px", top: "10px" }}
+    <Timeline>
+      {commentData.map((comment) => (
+        <Timeline.Item
+          key={comment.id}
+          sx={{
+            marginLeft: "0px",
+            "::before": {
+              left: "20px",
+              zIndex: "-1 ",
+            },
+          }}
         >
-          <Avatar src={user.avatarUrl} alt={user.name} />
-        </Timeline.Badge>
-        <Timeline.Body sx={{ zIndex: 1, backgroundColor: "white" }}>
-          <Box
-            borderWidth={1}
-            borderStyle="solid"
-            borderColor="border.default"
-            borderRadius={2}
+          <Timeline.Badge
+            sx={{
+              position: "absolute",
+              left: "-30px",
+              top: "10px",
+              width: "40px",
+              height: "40px",
+            }}
+          >
+            <Avatar
+              size={40}
+              src={comment.actor.avatar_url}
+              alt={comment.actor.login}
+            />
+          </Timeline.Badge>
+          <Timeline.Body
+            bg="bg.muted"
+            sx={{ zIndex: 1, backgroundColor: "white" }}
           >
             <Box
-              px={3}
-              py={2}
-              bg={getHeaderColor(role)}
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              borderBottom="1px solid"
+              borderWidth={1}
+              borderStyle="solid"
               borderColor="border.default"
-              borderTopLeftRadius={2}
-              borderTopRightRadius={2}
+              borderRadius={2}
             >
-              <Box>
-                <Text fontWeight="bold">{user.name}</Text>
-                {role && (
-                  <Text ml={2} color="fg.muted">
-                    {role}
+              <Box
+                px={3}
+                py={2}
+                bg={getHeaderColor(comment.actor.login)}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                borderBottom="1px solid"
+                borderColor="border.default"
+                borderTopLeftRadius={2}
+                borderTopRightRadius={2}
+              >
+                <Box>
+                  <Text fontWeight="bold">
+                    {comment.actor.login} commented{" "}
                   </Text>
-                )}
-                <Text ml={2} color="fg.muted">
-                  {time}
-                </Text>
-                {isEdited && (
-                  <Text ml={2} color="fg.muted">
-                    • edited by {editedBy}
-                  </Text>
-                )}
-              </Box>
-              {!isDeleted && (
+                  {comment.updated_at && (
+                    <Link href={comment.html_url}>
+                      <RelativeTime date={new Date(comment.updated_at)} />
+                    </Link>
+                  )}
+
+                  {comment.author_association && (
+                    <Label ml={2} color="fg.muted">
+                      {comment.author_association}
+                    </Label>
+                  )}
+                </Box>
+
                 <ActionMenu>
                   <ActionMenu.Button
                     aria-label="Actions"
                     sx={{
-                      '[data-component="trailingAction"]': { display: "none" },
+                      '[data-component="trailingAction"]': {
+                        display: "none",
+                      },
                       border: "none",
                       backgroundColor: "transparent",
                       boxShadow: "none",
@@ -112,71 +144,83 @@ const CommentItem = ({
                   >
                     <KebabHorizontalIcon />
                   </ActionMenu.Button>
-                  <ActionMenu.Overlay>
+                  <ActionMenu.Overlay width="medium">
                     <ActionList>
-                      <ActionList.Item>Delete</ActionList.Item>
-                      <ActionList.Item>Edit</ActionList.Item>
+                      <ActionList.Item>Copy link</ActionList.Item>
+                      <ActionList.Item>Quote reply</ActionList.Item>
+                      <ActionList.Item>Reference in new issue</ActionList.Item>
+                      <ActionList.Divider />
+                      <ActionList.Item
+                        onSelect={() => setEditingCommentId(comment.id)}
+                      >
+                        Edit
+                      </ActionList.Item>
+                      <ActionList.Item>Hide</ActionList.Item>
+                      <ActionList.Item
+                        variant="danger"
+                        onClick={() => handleDelete(comment.id)}
+                      >
+                        Delete
+                      </ActionList.Item>
+                      <ActionList.Divider />
+                      <ActionList.Item>Report content</ActionList.Item>
                     </ActionList>
                   </ActionMenu.Overlay>
                 </ActionMenu>
-              )}
+              </Box>
+              <Box>
+                {editingCommentId === comment.id ? (
+                  <>
+                    <CommentBox
+                      initialValue={comment.body}
+                      onTextareaChange={handleTextareaChange}
+                    />
+                    <Button
+                      variant="danger"
+                      onClick={() => setEditingCommentId(null)}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      variant="primary"
+                      onClick={() =>
+                        handleUpdate(comment.id, currentTextareaValue)
+                      }
+                    >
+                      Update comment
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Box p={3}>
+                      <Text>{comment.body}</Text>
+                    </Box>
+                    <Box px={3} pb={3} display="flex" alignItems="center">
+                      <Timeline.Badge
+                        variant="invisible"
+                        sx={{
+                          fontSixe: "14px",
+                          display: "flex",
+                          padding: "0px",
+                          width: "26px",
+                          height: "26px",
+                          marginLeft: "0",
+                        }}
+                      >
+                        <SmileyIcon />
+                      </Timeline.Badge>
+                    </Box>
+                  </>
+                )}
+              </Box>
             </Box>
+          </Timeline.Body>
+        </Timeline.Item>
+      ))}
 
-            <Box p={3}>
-              {isDeleted ? <Text color="fg.muted">This comment has been deleted</Text> : <Text>{content}</Text>}
-            </Box>
-            <Box px={3} pb={3} display="flex" alignItems="center">
-              <Timeline.Badge
-                variant="invisible"
-                sx={{
-                  fontSixe: "14px",
-                  display: "flex",
-                  padding: "0px",
-                  width: "26px",
-                  height: "26px",
-                  marginLeft: "0",
-                }}
-              >
-                <SmileyIcon />
-              </Timeline.Badge>
-            </Box>
-          </Box>
-        </Timeline.Body>
-      </Timeline.Item>
-    </>
+      <Timeline.Break />
+    </Timeline>
   );
 };
-
-const TimelineComment = () => (
-  <Timeline>
-    <CommentItem
-      user={{ name: "JuneLin2001", avatarUrl: "https://github.com/JuneLin2001.png" }}
-      role="Owner"
-      time="4 days ago"
-      content="try use api to get"
-    />
-    <CommentItem
-      user={{ name: "JuneLin2001", avatarUrl: "https://github.com/JuneLin2001.png" }}
-      role="Owner • Author"
-      time="4 days ago"
-      content="測試三hdinasd1!!!!!!!!!!!!!!"
-      isEdited
-      editedBy="rebeccaS47"
-    />
-    <CommentItem
-      user={{ name: "rebeccaS47", avatarUrl: "https://github.com/rebeccaS47.png" }}
-      time="yesterday"
-      content=""
-      isDeleted
-    />
-    <CommentItem
-      user={{ name: "hsin-2333", avatarUrl: "https://github.com/hsin-2333.png" }}
-      role="Collaborator"
-      time="15 minutes ago"
-      content="test"
-    />
-    <Timeline.Break />
-  </Timeline>
-);
 
 export default TimelineComment;
