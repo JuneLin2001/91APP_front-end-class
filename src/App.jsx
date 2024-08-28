bad395987c0f501e213f551368dc19b1adf1756c
Sylvie <yuanqi1368@example.com>
2024-08-28 10:20:07 +0800

    style: import primitive CSS variables (color, font, spacing...)

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import IssuePage from "./IssuePage.jsx";
 import GitHubLogin from "./utils/GitHubLogin";
 import CommentPage from "./CommentPage.jsx";
 import { AuthContextProvider } from "./context/authContext";
+import { ThemeProvider, BaseStyles } from "@primer/react";
 
 function App() {
   return (
-    <>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/login" element={<GitHubLogin />} />
-          <Route path="/:repoName/issue" element={<IssuePage />} />
-          <Route path="/comment/:issue_number" element={<CommentPage />} />
-        </Routes>
-      </AuthContextProvider>
-    </>
+    <ThemeProvider>
+      <BaseStyles>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/login" element={<GitHubLogin />} />
+            <Route path="/:repoName/issue" element={<IssuePage />} />
+            <Route path="/comment/:issue_number" element={<CommentPage />} />
+          </Routes>
+        </AuthContextProvider>
+      </BaseStyles>
+    </ThemeProvider>
   );
 }
 
