c947768e955d66e52b53eb912c682bbdd520a625
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-29 10:37:09 +0800

    fix: resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,32 @@ import { Routes, Route } from "react-router-dom";
 import IssuePage from "./IssuePage.jsx";
 import GitHubLogin from "./utils/GitHubLogin";
 import CommentPage from "./CommentPage.jsx";
+import PageLayoutComponent from "./PageLayout.jsx";
 import { AuthContextProvider } from "./context/authContext";
+import { CommentContextProvider } from "./context/commentContext.jsx";
 import { ThemeProvider, BaseStyles } from "@primer/react";
 
 function App() {
   return (
-    <>
-      <ThemeProvider>
-        <BaseStyles>
-          <AuthContextProvider>
-            <Routes>
-              <Route path="/login" element={<GitHubLogin />} />
-              <Route path="/:repoName/issue" element={<IssuePage />} />
-              <Route path="/comment/:issue_number" element={<CommentPage />} />
-            </Routes>
-          </AuthContextProvider>
-        </BaseStyles>
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <BaseStyles>
+        <AuthContextProvider>
+          <Routes>
+            <Route path="/login" element={<GitHubLogin />} />
+            <Route path="/:repoName/issue" element={<IssuePage />} />
+            <Route
+              path="/comment/:issueNumber"
+              element={
+                <CommentContextProvider>
+                  <CommentPage />
+                </CommentContextProvider>
+              }
+            />
+            <Route path="/page" element={<PageLayoutComponent />} />
+          </Routes>
+        </AuthContextProvider>
+      </BaseStyles>
+    </ThemeProvider>
   );
 }
 
--- src/CommentPage.jsx
+++ src/CommentPage.jsx
@@ -1,5 +1,21 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import {
+  Box,
+  Text,
+  ActionMenu,
+  ActionList,
+  ThemeProvider,
+  RelativeTime,
+  Button,
+  Pagehead,
+  PageHeader,
+  StateLabel,
+} from "@primer/react";
+import { IssueClosedIcon, MarkdownIcon } from "@primer/octicons-react";
+import { CommentContext } from "./context/commentContext";
+import CommentBox from "./comment";
+import TimelineComment from "./TimelineComment";
+import IssueBody from "./IssueBody";
 
 import {
   Box,
@@ -13,84 +29,80 @@ import {
   ThemeProvider,
   RelativeTime,
 } from "@primer/react";
+
 import { KebabHorizontalIcon } from "@primer/octicons-react";
 function CommentPage() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { issue_number } = useParams();
-
-  const owner = "JuneLin2001";
-  const repo = "91APP_front-end-class";
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const result = await response.json();
-        console.log("fetch到的資料", result);
-        setData(result);
-      } catch (e) {
-        setError(e.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [issue_number]);
+  const {
+    issueData,
+    commentData,
+    loading,
+    error,
+    currentTextareaValue,
+    handleTextareaChange,
+    handleCreateComment,
+  } = useContext(CommentContext);
 
   if (loading) return <div>載入中...</div>;
   if (error) return <div>錯誤: {error}</div>;
-  if (!data) return <div>無數據</div>;
+  if (!commentData) return <div>無數據</div>;
 
   return (
     <ThemeProvider>
-      <Timeline>
-        {data.map((comment) => (
-          <Timeline.Item key={comment.id}>
-            <Avatar size={40} src={comment.user.avatar_url} alt="User Avatar" />
-            <Timeline.Body ml={6} borderColor-accent-muted borderWidth={1}>
-              <Box>
-                <Text>{comment.user.login}</Text>
-                <RelativeTime date={new Date(comment.updated_at)} />
-                <Label>{comment.author_association}</Label>
-                <Label>Author</Label>
-                <ActionMenu>
-                  <ActionMenu.Anchor>
-                    <IconButton icon={KebabHorizontalIcon} unsafeDisableTooltip={false} variant="invisible" />
-                  </ActionMenu.Anchor>
-
-                  <ActionMenu.Overlay width="medium">
-                    <ActionList>
-                      <ActionList.Item onSelect={() => alert("Copy link clicked")}>Copy link</ActionList.Item>
-                      <ActionList.Item onSelect={() => alert("Quote reply clicked")}>Quote reply</ActionList.Item>
-                      <ActionList.Item onSelect={() => alert("Reference in new issue clicked")}>
-                        Reference in new issue
-                      </ActionList.Item>
-                      <ActionList.Divider />
-                      <ActionList.Item onSelect={() => alert("Edit comment clicked")}>Edit</ActionList.Item>
-                      <ActionList.Item onSelect={() => alert("Hide comment clicked")}>Hide</ActionList.Item>
-                      <ActionList.Item variant="danger" onSelect={() => alert("Delete comment clicked")}>
-                        Delete
-                      </ActionList.Item>
-                      <ActionList.Divider />
-                      <ActionList.Item onSelect={() => alert("Delete file clicked")}>Report content</ActionList.Item>
-                    </ActionList>
-                  </ActionMenu.Overlay>
-                </ActionMenu>
-              </Box>
-              <Box>
-                <Text>{comment.body}</Text>
-              </Box>
-            </Timeline.Body>
-          </Timeline.Item>
-        ))}
-      </Timeline>
+      <Pagehead>
+        <PageHeader.Title
+          sx={{
+            lineHeight: "1.25",
+            fontWeight: "normal",
+            fontSize: ["26px", "26px", "var(--text-title-size-large, 32px)"],
+          }}
+        >
+          {issueData.title}
+        </PageHeader.Title>
+        <p color="f1-light color-fg-muted">#{issueData.number}</p>
+        <StateLabel status="issueOpened">{issueData.state}</StateLabel>
+        <Text fontWeight="bold">{issueData.user.login}</Text>
+        <Text color="fg.muted"> opened this issue </Text>
+        <RelativeTime date={new Date(issueData.created_at)} color="fg.muted" />
+        <Text color="fg.muted"> · {issueData.comments} comments</Text>
+      </Pagehead>
+      <IssueBody />
+      <TimelineComment />
+      <h4>Add a comment </h4>
+      <CommentBox onTextareaChange={handleTextareaChange} />
+      <Box mt={3} display="flex" justifyContent="flex-end" alignItems="center">
+        <ActionMenu>
+          <ActionMenu.Button
+            aria-label="Close issue"
+            sx={{ display: "flex", alignItems: "center", mr: 2 }}
+          >
+            <IssueClosedIcon
+              sx={{
+                backgroundColor: "success.fg",
+                color: "fg.done",
+                mr: 3,
+                pr: 5,
+              }}
+              mr={1}
+              size={16}
+            />{" "}
+            Close issue
+          </ActionMenu.Button>
+          <ActionMenu.Overlay>
+            <ActionList>
+              <ActionList.Item>
+                <IssueClosedIcon className="color-fg-done" size={16} />
+                Close and cent
+              </ActionList.Item>
+            </ActionList>
+          </ActionMenu.Overlay>
+        </ActionMenu>
+        <Button
+          variant="primary"
+          onClick={() => handleCreateComment(currentTextareaValue)}
+        >
+          Comment
+        </Button>
+      </Box>
     </ThemeProvider>
   );
 }
