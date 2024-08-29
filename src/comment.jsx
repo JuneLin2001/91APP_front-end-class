05354ce2fa8a6d23f80dc5c9d3d6646db0b90b74
Sylvie <yuanqi1368@example.com>
2024-08-29 08:51:10 +0800

    style: modify `commentBox` UI

--- src/comment.jsx
+++ src/comment.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { Box, TabNav, Textarea, Text, IconButton } from "@primer/react";
+import { Box, TabNav, Textarea, Text, IconButton, Button } from "@primer/react";
 import {
   FileIcon,
   MarkdownIcon,
@@ -15,6 +15,7 @@ import {
   IssueClosedIcon,
   HeadingIcon,
   PaperclipIcon,
+  FileMediaIcon,
 } from "@primer/octicons-react";
 import PropType from "prop-types";
 
@@ -32,43 +33,149 @@ const CommentBox = ({ initialValue, onTextareaChange }) => {
     onTextareaChange(newValue);
   };
   return (
-    <Box borderWidth={1} borderStyle="solid" borderColor="border.default" borderRadius={2} p={3} bg="canvas.default">
+    <Box
+      borderWidth={1}
+      borderStyle="solid"
+      borderColor="border.default"
+      borderRadius={2}
+      bg="canvas.default"
+    >
       <TabNav aria-label="Comment">
-        <TabNav.Link selected={!isPreview} onClick={() => handleTabClick("Write")}>
+        <TabNav.Link
+          selected={!isPreview}
+          onClick={() => handleTabClick("Write")}
+        >
           Write
         </TabNav.Link>
-        <TabNav.Link selected={isPreview} onClick={() => handleTabClick("Preview")}>
+        <TabNav.Link
+          selected={isPreview}
+          onClick={() => handleTabClick("Preview")}
+        >
           Preview
         </TabNav.Link>
         <Box>
-          <IconButton icon={HeadingIcon} aria-label="Attach files" mr={2} />
-          <IconButton icon={BoldIcon} aria-label="Bold" mr={2} />
-          <IconButton icon={ItalicIcon} aria-label="Italic" mr={2} />
-          <IconButton icon={CodeIcon} aria-label="Code" mr={2} />
-          <IconButton icon={LinkIcon} aria-label="Link" mr={2} />
-          <IconButton icon={ListUnorderedIcon} aria-label="Unordered list" mr={2} />
-          <IconButton icon={ListOrderedIcon} aria-label="Ordered list" mr={2} />
-          <IconButton icon={QuoteIcon} aria-label="Quote" mr={2} />
-          <IconButton icon={PaperclipIcon} aria-label="Data files attachment" mr={2} />
-          <IconButton icon={MentionIcon} aria-label="Mention" mr={2} />
+          <IconButton
+            icon={HeadingIcon}
+            variant="invisible"
+            aria-label="Attach files"
+            mr={2}
+          />
+          <IconButton
+            icon={BoldIcon}
+            variant="invisible"
+            aria-label="Bold"
+            mr={2}
+          />
+          <IconButton
+            icon={ItalicIcon}
+            variant="invisible"
+            aria-label="Italic"
+            mr={2}
+          />
+          <IconButton
+            icon={CodeIcon}
+            variant="invisible"
+            aria-label="Code"
+            mr={2}
+          />
+          <IconButton
+            icon={LinkIcon}
+            variant="invisible"
+            aria-label="Link"
+            mr={2}
+          />
+          <IconButton
+            icon={ListUnorderedIcon}
+            variant="invisible"
+            aria-label="Unordered list"
+            mr={2}
+          />
+          <IconButton
+            icon={ListOrderedIcon}
+            variant="invisible"
+            aria-label="Ordered list"
+            mr={2}
+          />
+          <IconButton
+            icon={QuoteIcon}
+            variant="invisible"
+            aria-label="Quote"
+            mr={2}
+          />
+          <IconButton
+            icon={PaperclipIcon}
+            variant="invisible"
+            aria-label="Data files attachment"
+            mr={2}
+          />
+          <IconButton
+            icon={MentionIcon}
+            variant="invisible"
+            aria-label="Mention"
+            mr={2}
+          />
         </Box>
       </TabNav>
 
-      <Box mt={3}>
+      <Box m={2}>
         {!isPreview ? (
-          <Textarea
-            placeholder="Add your comment here..."
-            aria-label="Comment"
-            value={inputValue}
-            onChange={handleInputChange}
-            sx={{ width: "100%", height: "100%", resize: "none" }}
-          />
+          <>
+            <Textarea
+              placeholder="Add your comment here..."
+              aria-label="Comment"
+              value={inputValue}
+              onChange={handleInputChange}
+              sx={{ width: "100%", height: "100%", resize: "none" }}
+            />
+            <Box
+              mt={2}
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+              }}
+            >
+              <Box>
+                <Button
+                  variant="invisible"
+                  size="small"
+                  leadingVisual={MarkdownIcon}
+                >
+                  Markdown is supported
+                </Button>
+              </Box>
+              <Box
+                role="separator"
+                mx={1}
+                sx={{
+                  width: 1,
+                  backgroundColor: "#e1e4e8",
+                }}
+              ></Box>
+              <Box>
+                <Button
+                  variant="invisible"
+                  size="small"
+                  leadingVisual={FileMediaIcon}
+                >
+                  Paste, drop, or click to add files
+                </Button>
+              </Box>
+            </Box>
+          </>
         ) : (
-          <Box p={3} borderColor="border.default" borderWidth={1} borderStyle="solid">
+          <Box
+            p={3}
+            borderColor="border.default"
+            borderWidth={1}
+            borderStyle="solid"
+          >
             {inputValue.trim() ? (
               <Text>{inputValue}</Text>
             ) : (
-              <Text color="fg.muted" sx={{ width: "400px", height: "150px", resize: "none" }}>
+              <Text
+                color="fg.muted"
+                sx={{ width: "400px", height: "150px", resize: "none" }}
+              >
                 Nothing to preview
               </Text>
             )}
