9043a845abb4c7a2c78a32f20778f048e850e632
Yen-Chun,Lin <asd900606@gmail.com>
2024-08-29 10:15:21 +0800

    Revert "fix: remove useless component"
    This reverts commit 7d6038f4e7ef1a2b46a564430e21a4ad09a67ba2.

---
+++ src/context/issueContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState, useContext } from "react";
+
+// 創建 Context
+const IssueContext = createContext();
+
+// Context 提供者
+export const IssueProvider = ({ children }) => {
+  const [selectedIssue, setSelectedIssue] = useState({ title: "", body: "" });
+
+  return (
+    <IssueContext.Provider value={{ selectedIssue, setSelectedIssue }}>
+      {children}
+    </IssueContext.Provider>
+  );
+};
+
+// 自定義 hook 來使用 IssueContext
+export const useIssue = () => {
+  return useContext(IssueContext);
+};
