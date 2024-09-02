6d1845cdcadd611561bcb1dba7333144c23a2a48
Yen-Chun,Lin <asd900606@gmail.com>
2024-09-02 18:06:05 +0800

    feat: add IssuePageNewIssueAddLabel button

---
+++ src/Pages/IssuePage/IssuePageNewIssueAddLabel.jsx
@@ -0,0 +1,70 @@
+import { useState, useRef } from "react";
+import { TriangleDownIcon } from "@primer/octicons-react";
+import { SelectPanel, Button } from "@primer/react";
+
+const IssuePageNewIssueAddLabel = ({ labels = [], onSelect }) => {
+  const [selected, setSelected] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [open, setOpen] = useState(false);
+  const buttonRef = useRef(null);
+
+  const filteredItems = labels
+    .filter((label) =>
+      label.name.toLowerCase().startsWith(filter.toLowerCase())
+    )
+    .map((label) => ({
+      key: label.id,
+      text: label.name,
+      description: label.description,
+      color: `#${label.color}`,
+    }));
+
+  const handleSelectedChange = (selectedItems) => {
+    const selectedNames = selectedItems.map((item) => item.text);
+    setSelected(selectedNames);
+    if (onSelect) {
+      onSelect(selectedNames);
+    }
+  };
+
+  return (
+    <>
+      <SelectPanel
+        renderAnchor={({
+          //   children,
+          "aria-labelledby": ariaLabelledBy,
+          ...anchorProps
+        }) => (
+          <Button
+            ref={buttonRef}
+            variant="invisible"
+            trailingAction={TriangleDownIcon}
+            aria-labelledby={` ${ariaLabelledBy}`}
+            {...anchorProps}
+          >
+            {"Label"}
+          </Button>
+        )}
+        placeholderText="Filter Labels"
+        open={open}
+        onOpenChange={setOpen}
+        items={filteredItems}
+        selected={filteredItems.filter((item) => selected.includes(item.text))}
+        onSelectedChange={handleSelectedChange}
+        onFilterChange={setFilter}
+        showItemDividers={true}
+        overlayProps={{
+          width: "small",
+          height: "medium",
+        }}
+        footer={
+          <Button size="small" block>
+            Edit labels
+          </Button>
+        }
+      />
+    </>
+  );
+};
+
+export default IssuePageNewIssueAddLabel;
